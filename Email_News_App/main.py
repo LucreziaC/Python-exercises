a81d86a285806269105784c5f5206ac933adba37
Lucrezia Carena <lucrezia.carena@gmail.com>
2026-01-30 11:58:15 +0100

    fix: url quotes

--- Email_News_App/main.py
+++ Email_News_App/main.py
@@ -1,27 +1,36 @@
 import requests
 import send_email
 
-topic="tesla"
+topic = "tesla"
 
-apy_key = '1e6a9fc56a64418681dc17a9c7939781'
+apy_key = "1e6a9fc56a64418681dc17a9c7939781"
 
-url = 'https://newsapi.org/v2/everything?"\
-    "q={topic}&"\
-        "from=2025-12-28" \
-    "&sortBy=publishedAt&apiKey=1e6a9fc56a64418681dc17a9c7939781&language=en'
+url = (
+    "https://newsapi.org/v2/everything?"
+    "q={topic}&"
+    "from=2025-12-28"
+    "&sortBy=publishedAt&apiKey=1e6a9fc56a64418681dc17a9c7939781&language=en"
+)
 
-#Make request
-request= requests.get(url)
+# Make request
+request = requests.get(url)
 
 # get a dictionary with data
-content= request.json()
+content = request.json()
 
-body=""
+body = ""
 for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = "Subject: Today's news" + body + article["title"]+"\n" \
-            + article["description"] \
-                + "\n" + article["url"] + 2*"\n"
-    
-body= body.encode("utf-8")
+        body = (
+            "Subject: Today's news"
+            + body
+            + article["title"]
+            + "\n"
+            + article["description"]
+            + "\n"
+            + article["url"]
+            + 2 * "\n"
+        )
+
+body = body.encode("utf-8")
 send_email.send_email(message=body)

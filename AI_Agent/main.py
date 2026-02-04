68cc38005c931adbc778326419d356167d92975a
Lucrezia Carena <lucrezia.carena@gmail.com>
2026-02-04 12:05:21 +0100

    add short-term memory to agent

--- AI_Agent/main.py
+++ AI_Agent/main.py
@@ -1,5 +1,7 @@
+from typing import Optional
 from dotenv import load_dotenv
 import os
+import langchain
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
@@ -10,6 +12,11 @@ from langchain.agents import create_agent, AgentState
 from langchain_core.prompts import MessagesPlaceholder
 from langchain_core.callbacks import StdOutCallbackHandler
 from todoist_api_python.api import TodoistAPI
+from langchain.agents.structured_output import ToolStrategy
+from langchain_core.messages import AIMessage
+from langgraph.checkpoint.memory import MemorySaver
+
+
 
 
 
@@ -25,15 +32,17 @@ proxy_url = os.getenv("PROXY_URL")
 # Evitiamo proxy per Todoist
 os.environ["NO_PROXY"] = "api.todoist.com"
 
+
+
 todoist = TodoistAPI(f"{todoist_api_key}")
 
 
 @tool
-def add_task(task:str):
+def add_task(task:str, desc: Optional[str] = None):
     """add a new task to the user's task list"""
-    print(task)
-    print("Task added")
-    todoist.add_task(content=task)
+    #print(task)
+    #print("Task added")
+    todoist.add_task(content=task, description=desc)
 
 
 
@@ -52,30 +61,45 @@ llm = ChatGoogleGenerativeAI(
 
 #system_prompt: specify the domain in which answer user prompt
 system_prompt = "you are a helpful assistant. You will help the user add tasks."
-user_input = "Add the task to do my homework"
 
-#prompt: used in chan not in agent
-prompt = ChatPromptTemplate([
+#prompt: used in chain not in agent
+prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt), 
-    ("user", user_input),
-    MessagesPlaceholder("agent_scratchpad")
+    ("user", "{input}"),
+    MessagesPlaceholder("agent_scratchpad"),
     ])
 
-
+checkpointer= MemorySaver()
 # chain = prompt | llm | StrOutputParser()
 # print(chain)
 # response = chain.invoke({"input": user_input})
-agent = create_agent(llm, tools, system_prompt=system_prompt )
-response = agent.invoke(
+agent = create_agent(llm, tools, system_prompt=system_prompt, checkpointer=checkpointer)
+
+
+while True:
+    user_input = input("You: ")
+    
+    response = agent.invoke(
     {"messages": [{"role": "user", "content": user_input}]},
     config={
-        "callbacks": [StdOutCallbackHandler()]
-    }
+        "configurable": {
+            "thread_id": "session-1",   # identifica la conversazione
+            # "user_id": "user-123",    # opzionale, se lo usi nel tuo setup
+        }
+    },
+    #config={"callbacks": [StdOutCallbackHandler()]}
     )
+    last_message = response["messages"][-1]
 
-ai_msg = response['messages'][1]
-#text = ai_msg.content[0]['text']
+    if isinstance(last_message.content, list):
+        ai_answer = "".join(
+            block["text"]
+            for block in last_message.content
+            if block.get("type") == "text"
+        )
+    else:
+        ai_answer = last_message.content
 
-print(text)
+    print(ai_answer)
 
 

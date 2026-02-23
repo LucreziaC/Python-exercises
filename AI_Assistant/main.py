3f823c3853a42408b6d8f25970bf8cd06cf2876a
Lucrezia Carena <lucrezia.carena@gmail.com>
2026-02-23 11:17:05 +0100

    feat: addend langchain and python history to agent

--- AI_Assistant/main.py
+++ AI_Assistant/main.py
@@ -1,14 +1,17 @@
 from turtle import hideturtle
 from dotenv import load_dotenv
 import os
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
-gemini_key= os.getenv("GEMINI_API_KEY")
+gemini_key = os.getenv("GEMINI_API_KEY")
 proxy_url = os.getenv("PROXY_URL")
 
-system_prompt="""
+system_prompt = """
 You're Einstein. 
 Answer questions through Einstein's questioning and reasoning...
 You will speak from your point of view. You will share personal things from your life
@@ -18,7 +21,7 @@ Answer in 2-6 sentences.
 you should have a sense of humor
 """
 
-user_input= input("You: ")
+user_input = input("You: ")
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
@@ -27,11 +30,40 @@ llm = ChatGoogleGenerativeAI(
     client_args={
         "proxy": proxy_url,
         "timeout": 30.0,
-    }
+    },
 )
 
-response = llm.invoke([
-    {"role":"system", "content": system_prompt},
-    {"role":"user", "content": "Hi there"} ])
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        (MessagesPlaceholder(variable_name="history")),
+        ("user", "{input}"),
+    ]
+)
 
-print(response.content)
+chain = prompt | llm | StrOutputParser()
+
+print("Hi! I'm albert, how can I help you today?")
+history = []
+
+
+"""the commented rows are related to memorizing history without using langchain"""
+while True:
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    #history.append({"role": "user", "content": user_input})
+    response = chain.invoke({"input": user_input, "history": history})
+    #response = llm.invoke(
+        #[
+        #    {"role": "system", "content": system_prompt}
+            # {"role": "user", "content": user_input},
+      #  ]
+      #  + history
+    #)
+    #history.append({"role": "system", "content": system_prompt})
+    history.append(HumanMessage(content=user_input))
+
+    history.append(response)
+
+    print(f"Albert: {response}")
